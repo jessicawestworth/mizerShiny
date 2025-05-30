8f0854c631a1ba54dc2c3578bfba31f9da13b4f4
LucaBroadbentCefas <luke.broadbent@cefas.gov.uk>
2025-05-30 12:07:08 +0100

    1

--- inst/app/Functions/percentdiff.R
+++ inst/app/Functions/percentdiff.R
@@ -1,22 +1,4 @@
-#' MizerSim Relative Biomass per Species 
-#'
-#' This calculates the percentage difference between the value of biomass 
-#' that are separated by a Species column.
-#'
-#' @param harvested An array (time x species)
-#' @param unharvested An array (time x species), the value you are comparing to.
-#'
-#' @return A dataframe of Species and Biomass. Biomass gives the percentage
-#' difference of the value of biomass between the harvested and unharvested
-#' mizerSim objects.
-#' 
-#'
-#' @examples
-#' harvested <- getBiomass(NS_sim)
-#' unharvested <- getBiomass(NS_sim)
-#' percentdiff(harvested, unharvested)
-#'
-#' @export
+
 percentdiff <- function(harvested, unharvested) {
   harvested %>%
     left_join(unharvested, by = "Species") %>%
