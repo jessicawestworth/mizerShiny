69406b5f747eee6e90b3cf663cb4771a7f378c82
LucaBroadbentCefas <luke.broadbent@cefas.gov.uk>
2025-06-03 11:02:11 +0100

    Fixed functionality

--- R/launchApp.R
+++ R/launchApp.R
@@ -40,22 +40,28 @@ mizerShiny <- function(mizerParams = NULL,
                        REPLACE     = FALSE,
                        ...) {
 
-  ## ---- load default params from Including/mizerParam if none given ----
-  params_file <- system.file("Including", "mizerParam", "params.rds",
-                             package = "mizerShiny")
-  if (is.null(mizerParams) && file.exists(params_file)) {
-    mizerParams <- readRDS(params_file)
+  ## IS there the location of the files
+  app_root <- system.file("app", package = "mizerShiny")
+  if (app_root == "") {
+    stop("Could not find installed 'app' directory. Please reinstall 'mizerShiny'.", call. = FALSE)
+  }
+  including_dir <- file.path(app_root, "Including")
+  params_path   <- file.path(including_dir, "mizerParam", "params.rds")
+
+  ## Is there a mizerParams object, either loaded in on function or internally.
+  if (is.null(mizerParams) && file.exists(params_path)) {
+    mizerParams <- readRDS(params_path)
   } else if (is.null(mizerParams)) {
     mizerParams <- mizer::NS_params
   }
 
-  ## ---- helper to save an object in the expected on-disk location -----------
+  ## helper to save files (for REPLACE)
   save_obj <- function(obj, subdir, fname) {
-    dir.create(app_path("Including", subdir), recursive = TRUE, showWarnings = FALSE)
-    save(obj, file = app_path("Including", subdir, fname), compress = "xz")
+    dir_to <- file.path(including_dir, subdir)
+    dir.create(dir_to, recursive = TRUE, showWarnings = FALSE)
+    save(obj, file = file.path(dir_to, fname), compress = "xz")
   }
 
-  ## ---- set up the working objects exactly as before ------------------------
   celticsim             <- NULL
   unharvestedprojection <- NULL
   unfishedprojection    <- NULL
@@ -63,20 +69,25 @@ mizerShiny <- function(mizerParams = NULL,
   # 1. handle MizerParams -----------------------------------------------------
   if (inherits(mizerParams, "MizerParams")) {
     celticsim <- mizerParams
-    if (isTRUE(REPLACE))
-      save_obj(mizerParams, "mizerParam", "params.rds")
+    if (isTRUE(REPLACE)) {
+      # overwrite the installed params.rds
+      dir.create(dirname(params_path), recursive = TRUE, showWarnings = FALSE)
+      saveRDS(mizerParams, file = params_path)
+    }
   }
 
   # 2. handle baseline sims ---------------------------------------------------
   if (inherits(baseSpSim, "MizerSim")) {
     unharvestedprojection <- baseSpSim
-    if (isTRUE(REPLACE))
+    if (isTRUE(REPLACE)) {
       save_obj(baseSpSim, "mizerSim/Unharvested", "unharvestedprojection.rdata")
+    }
   }
   if (inherits(baseFishSim, "MizerSim")) {
     unfishedprojection <- baseFishSim
-    if (isTRUE(REPLACE))
+    if (isTRUE(REPLACE)) {
       save_obj(baseFishSim, "mizerSim/Unfished", "unfishedprojection.rdata")
+    }
   }
 
   ## ---- make them visible to the app without touching GlobalEnv -------------
@@ -93,12 +104,14 @@ mizerShiny <- function(mizerParams = NULL,
 
   ## ---- locate the Shiny app -------------------------------------------------
   dev_path <- file.path("inst", "app", "mizerShinyApp.R")
-  if (file.exists(dev_path))
+  if (file.exists(dev_path)) {
     return(shiny::runApp(dev_path, ...))
+  }
 
   pkg_app <- system.file("app", "mizerShinyApp.R", package = "mizerShiny")
-  if (pkg_app == "")
+  if (pkg_app == "") {
     stop("Could not find Shiny app; reinstall or rebuild `mizerShiny`.", call. = FALSE)
+  }
 
   shiny::runApp(pkg_app, ...)
 }
